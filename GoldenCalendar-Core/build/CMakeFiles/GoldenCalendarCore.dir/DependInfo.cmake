f3d21c5c75bca729792c15b31369972939d371ae
Nemimeche Benaissa <nemimeche.benaissa@gmail.com>
2026-07-18 13:42:53 +0100

    Add DayEngine and SolarEngine core modules

---
+++ GoldenCalendar-Core/build/CMakeFiles/GoldenCalendarCore.dir/DependInfo.cmake
@@ -0,0 +1,28 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/data/data/com.termux/files/home/GoldenCalendar_Project/golden-calendar/GoldenCalendar-Core/src/ChronologyEngine.cpp" "CMakeFiles/GoldenCalendarCore.dir/src/ChronologyEngine.cpp.o" "gcc" "CMakeFiles/GoldenCalendarCore.dir/src/ChronologyEngine.cpp.o.d"
+  "/data/data/com.termux/files/home/GoldenCalendar_Project/golden-calendar/GoldenCalendar-Core/src/DayEngine.cpp" "CMakeFiles/GoldenCalendarCore.dir/src/DayEngine.cpp.o" "gcc" "CMakeFiles/GoldenCalendarCore.dir/src/DayEngine.cpp.o.d"
+  "/data/data/com.termux/files/home/GoldenCalendar_Project/golden-calendar/GoldenCalendar-Core/src/Epoch.cpp" "CMakeFiles/GoldenCalendarCore.dir/src/Epoch.cpp.o" "gcc" "CMakeFiles/GoldenCalendarCore.dir/src/Epoch.cpp.o.d"
+  "/data/data/com.termux/files/home/GoldenCalendar_Project/golden-calendar/GoldenCalendar-Core/src/SolarEngine.cpp" "CMakeFiles/GoldenCalendarCore.dir/src/SolarEngine.cpp.o" "gcc" "CMakeFiles/GoldenCalendarCore.dir/src/SolarEngine.cpp.o.d"
+  "/data/data/com.termux/files/home/GoldenCalendar_Project/golden-calendar/GoldenCalendar-Core/src/main.cpp" "CMakeFiles/GoldenCalendarCore.dir/src/main.cpp.o" "gcc" "CMakeFiles/GoldenCalendarCore.dir/src/main.cpp.o.d"
+  "" "GoldenCalendarCore" "gcc" "CMakeFiles/GoldenCalendarCore.dir/link.d"
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_LINKED_INFO_FILES
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_FORWARD_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
