f1a2075ca8a9bd2a5ec4a7902cd6f8c720163937
Nemimeche Benaissa <nemimeche.benaissa@gmail.com>
2026-07-18 22:20:34 +0100

    Add long range solar epoch test 2083 years

--- GoldenCalendar-Core/src/main.cpp
+++ GoldenCalendar-Core/src/main.cpp
@@ -1,51 +1,49 @@
 #include <iostream>
 
 #include "../include/ChronologyEngine.h"
+#include "../include/SolarEngine.h"
 
 
 int main()
 {
 
+    long long dayId =
+        SolarEngine::toDayId(2083,12,31);
+
+
+    std::cout
+    << "Day ID for 31 December 2083: "
+    << dayId
+    << "\n\n";
+
+
     ChronologyEngine engine;
 
 
-    long long tests[] =
-    {
-        1,
-        32,
-        366,
-        1155
-    };
+    CalendarDay day =
+        engine.getDay(dayId);
 
 
-    for(long long id : tests)
-    {
-
-        CalendarDay day =
-            engine.getDay(id);
-
-
-        std::cout
-        << "Day ID: "
-        << day.dayId
-        << "\n";
+    std::cout
+    << "Day ID: "
+    << day.dayId
+    << "\n";
 
 
-        std::cout
-        << "Weekday: "
-        << day.weekday
-        << "\n";
+    std::cout
+    << "Weekday: "
+    << day.weekday
+    << "\n";
 
 
-        std::cout
-        << "Solar: "
-        << day.solar.day
-        << " "
-        << day.solar.monthName
-        << " "
-        << day.solar.year
-        << "\n\n";
-    }
+    std::cout
+    << "Solar: "
+    << day.solar.day
+    << " "
+    << day.solar.monthName
+    << " "
+    << day.solar.year
+    << "\n";
 
 
     return 0;
