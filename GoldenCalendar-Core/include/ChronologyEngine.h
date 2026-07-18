2d2d55fb1420a08c46636b0b9bf7725266959df8
Nemimeche Benaissa <nemimeche.benaissa@gmail.com>
2026-07-18 23:40:35 +0100

    Connect ChronologyEngine with EpochGenerator

--- GoldenCalendar-Core/include/ChronologyEngine.h
+++ GoldenCalendar-Core/include/ChronologyEngine.h
@@ -2,8 +2,15 @@
 #define CHRONOLOGY_ENGINE_H
 
 #include "Date.h"
+#include "EpochSegment.h"
+#include <vector>
 
 class ChronologyEngine {
+
+private:
+
+    std::vector<EpochSegment> epochs;
+
 
 public:
 
--- GoldenCalendar-Core/src/ChronologyEngine.cpp
+++ GoldenCalendar-Core/src/ChronologyEngine.cpp
@@ -2,36 +2,35 @@
 #include "../include/Epoch.h"
 #include "../include/DayEngine.h"
 #include "../include/SolarEngine.h"
+#include "../include/EpochGenerator.h"
 
 
 ChronologyEngine::ChronologyEngine()
 {
-
+    epochs =
+        EpochGenerator::generate(
+            50000,
+            2083
+        );
 }
 
 
 CalendarDay ChronologyEngine::getDay(long long dayId)
 {
-
     if(dayId == 1)
     {
         return Epoch::firstDay();
     }
 
-
     CalendarDay day;
 
     day.dayId = dayId;
 
-
     day.weekday =
         DayEngine::getWeekday(dayId);
-
 
     day.solar =
         SolarEngine::getDate(dayId);
 
-
     return day;
-
 }
