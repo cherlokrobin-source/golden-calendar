ab9c7c06a393fac157e3211d63e451dbb4a07a0e
Nemimeche Benaissa <nemimeche.benaissa@gmail.com>
2026-07-18 22:24:41 +0100

    Add EpochSegment structure for timeline partitioning

---
+++ GoldenCalendar-Core/include/EpochSegment.h
@@ -0,0 +1,17 @@
+#ifndef EPOCH_SEGMENT_H
+#define EPOCH_SEGMENT_H
+
+struct EpochSegment
+{
+
+    long long startYear;
+
+    long long endYear;
+
+    long long startDayId;
+
+    long long endDayId;
+
+};
+
+#endif
